0c61414cb40e306d6cb0091b936dea869819f763
amel <amel.nait-amer@etu.u-paris.fr>
2024-05-16 01:48:57 +0200

    Test the impact of removing SIF on the performance

--- data_loader.py
+++ data_loader.py
@@ -2,7 +2,6 @@
 
 import os
 from collections import Counter
-
 import nltk
 import numpy as np
 import scipy.io
@@ -10,16 +9,18 @@ from sklearn.decomposition import PCA, TruncatedSVD
 from sklearn.preprocessing import MinMaxScaler
 
 
-def load_stackoverflow(data_path='data/stackoverflow/'):
+def load_stackoverflow(data_path="data/stackoverflow/", use_SIF=True):
 
     # load SO embedding
-    with open(data_path + 'vocab_withIdx.dic', 'r') as inp_indx, \
-            open(data_path + 'vocab_emb_Word2vec_48_index.dic', 'r') as inp_dic, \
-            open(data_path + 'vocab_emb_Word2vec_48.vec') as inp_vec:
+    with open(data_path + "vocab_withIdx.dic", "r", encoding="utf-8") as inp_indx, open(
+        data_path + "vocab_emb_Word2vec_48_index.dic", "r", encoding="utf-8"
+    ) as inp_dic, open(
+        data_path + "vocab_emb_Word2vec_48.vec", encoding="utf-8"
+    ) as inp_vec:
         pair_dic = inp_indx.readlines()
         word_index = {}
         for pair in pair_dic:
-            word, index = pair.replace('\n', '').split('\t')
+            word, index = pair.replace("\n", "").split("\t")
             word_index[word] = index
 
         index_word = {v: k for k, v in word_index.items()}
@@ -30,13 +31,13 @@ def load_stackoverflow(data_path='data/stackoverflow/'):
         emb_vec = inp_vec.readlines()
         word_vectors = {}
         for index, vec in zip(emb_index, emb_vec):
-            word = index_word[index.replace('\n', '')]
+            word = index_word[index.replace("\n", "")]
             word_vectors[word] = np.array(list((map(float, vec.split()))))
 
         del emb_index
         del emb_vec
 
-    with open(data_path + 'title_StackOverflow.txt', 'r') as inp_txt:
+    with open(data_path + "title_StackOverflow.txt", "r", encoding="utf-8") as inp_txt:
         all_lines = inp_txt.readlines()[:-1]
         text_file = " ".join([" ".join(nltk.word_tokenize(c)) for c in all_lines])
         word_count = Counter(text_file.split())
@@ -49,57 +50,68 @@ def load_stackoverflow(data_path='data/stackoverflow/'):
         for i, line in enumerate(all_lines):
             word_sentence = nltk.word_tokenize(line)
 
-            sent_rep = np.zeros(shape=[48, ])
+            sent_rep = np.zeros(shape=(48,))
             j = 0
+
             for word in word_sentence:
                 try:
                     wv = word_vectors[word]
                     j = j + 1
                 except KeyError:
                     continue
-
-                weight = 0.1 / (0.1 + unigram[word])
-                sent_rep += wv * weight
+                if use_SIF:
+                    weight = 0.1 / (0.1 + unigram[word])
+                    sent_rep += wv * weight
+                else:
+                    sent_rep += wv
             if j != 0:
                 all_vector_representation[i] = sent_rep / j
             else:
                 all_vector_representation[i] = sent_rep
 
-    pca = PCA(n_components=1)
-    pca.fit(all_vector_representation)
-    pca = pca.components_
+    if use_SIF:
+        pca = PCA(n_components=1)
+        pca.fit(all_vector_representation)
+        pca = pca.components_
 
-    XX1 = all_vector_representation - all_vector_representation.dot(pca.transpose()) * pca
+        XX1 = (
+            all_vector_representation
+            - all_vector_representation.dot(pca.transpose()) * pca
+        )
 
-    XX = XX1
+        XX = XX1
 
-    scaler = MinMaxScaler()
-    XX = scaler.fit_transform(XX)
+        scaler = MinMaxScaler()
+        XX = scaler.fit_transform(XX)
+    else:
+        XX = all_vector_representation
 
-    with open(data_path + 'label_StackOverflow.txt') as label_file:
+    with open(data_path + "label_StackOverflow.txt", encoding="utf-8") as label_file:
         y = np.array(list((map(int, label_file.readlines()))))
         print(y.dtype)
 
     return XX, y
 
 
-def load_search_snippet2(data_path='data/SearchSnippets/'):
-    mat = scipy.io.loadmat(data_path + 'SearchSnippets-STC2.mat')
+def load_search_snippet2(data_path="data/SearchSnippets/", use_SIF=True):
+    mat = scipy.io.loadmat(data_path + "SearchSnippets-STC2.mat")
 
-    emb_index = np.squeeze(mat['vocab_emb_Word2vec_48_index'])
-    emb_vec = mat['vocab_emb_Word2vec_48']
-    y = np.squeeze(mat['labels_All'])
+    emb_index = np.squeeze(mat["vocab_emb_Word2vec_48_index"])
+    emb_vec = mat["vocab_emb_Word2vec_48"]
+    y = np.squeeze(mat["labels_All"])
 
     del mat
 
     rand_seed = 0
 
     # load SO embedding
-    with open(data_path + 'SearchSnippets_vocab2idx.dic', 'r') as inp_indx:
+    with open(
+        data_path + "SearchSnippets_vocab2idx.dic", "r", encoding="utf-8"
+    ) as inp_indx:
         pair_dic = inp_indx.readlines()
         word_index = {}
         for pair in pair_dic:
-            word, index = pair.replace('\n', '').split('\t')
+            word, index = pair.replace("\n", "").split("\t")
             word_index[word] = index
 
         index_word = {v: k for k, v in word_index.items()}
@@ -114,7 +126,7 @@ def load_search_snippet2(data_path='data/SearchSnippets/'):
         del emb_index
         del emb_vec
 
-    with open(data_path + 'SearchSnippets.txt', 'r') as inp_txt:
+    with open(data_path + "SearchSnippets.txt", "r", encoding="utf-8") as inp_txt:
         all_lines = inp_txt.readlines()[:-1]
         all_lines = [line for line in all_lines]
         text_file = " ".join([" ".join(nltk.word_tokenize(c)) for c in all_lines])
@@ -128,7 +140,7 @@ def load_search_snippet2(data_path='data/SearchSnippets/'):
         for i, line in enumerate(all_lines):
             word_sentence = nltk.word_tokenize(line)
 
-            sent_rep = np.zeros(shape=[48, ])
+            sent_rep = np.zeros(shape=(48,))
             j = 0
             for word in word_sentence:
                 try:
@@ -136,45 +148,55 @@ def load_search_snippet2(data_path='data/SearchSnippets/'):
                     j = j + 1
                 except KeyError:
                     continue
-
-                weight = 0.1 / (0.1 + unigram[word])
-                sent_rep += wv * weight
+                if use_SIF:
+                    weight = 0.1 / (0.1 + unigram[word])
+                    sent_rep += wv * weight
+                else:
+                    sent_rep += wv
             if j != 0:
                 all_vector_representation[i] = sent_rep / j
             else:
                 all_vector_representation[i] = sent_rep
 
-    svd = TruncatedSVD(n_components=1, n_iter=20)
-    svd.fit(all_vector_representation)
-    svd = svd.components_
+    if use_SIF:
+        svd = TruncatedSVD(n_components=1, n_iter=20)
+        svd.fit(all_vector_representation)
+        svd = svd.components_
 
-    XX = all_vector_representation - all_vector_representation.dot(svd.transpose()) * svd
+        XX = (
+            all_vector_representation
+            - all_vector_representation.dot(svd.transpose()) * svd
+        )
 
-    scaler = MinMaxScaler()
-    XX = scaler.fit_transform(XX)
+        scaler = MinMaxScaler()
+        XX = scaler.fit_transform(XX)
+    else:
+        XX = all_vector_representation
 
     return XX, y
 
 
-def load_biomedical(data_path='data/Biomedical/'):
-    mat = scipy.io.loadmat(data_path + 'Biomedical-STC2.mat')
+def load_biomedical(data_path="data/Biomedical/", use_SIF=True):
+    mat = scipy.io.loadmat(data_path + "Biomedical-STC2.mat")
 
-    emb_index = np.squeeze(mat['vocab_emb_Word2vec_48_index'])
-    emb_vec = mat['vocab_emb_Word2vec_48']
-    y = np.squeeze(mat['labels_All'])
+    emb_index = np.squeeze(mat["vocab_emb_Word2vec_48_index"])
+    emb_vec = mat["vocab_emb_Word2vec_48"]
+    y = np.squeeze(mat["labels_All"])
 
     del mat
 
     rand_seed = 0
 
     # load SO embedding
-    with open(data_path + 'Biomedical_vocab2idx.dic', 'r') as inp_indx:
+    with open(
+        data_path + "Biomedical_vocab2idx.dic", "r", encoding="utf-8"
+    ) as inp_indx:
         # open(data_path + 'vocab_emb_Word2vec_48_index.dic', 'r') as inp_dic, \
         # open(data_path + 'vocab_emb_Word2vec_48.vec') as inp_vec:
         pair_dic = inp_indx.readlines()
         word_index = {}
         for pair in pair_dic:
-            word, index = pair.replace('\n', '').split('\t')
+            word, index = pair.replace("\n", "").split("\t")
             word_index[word] = index
 
         index_word = {v: k for k, v in word_index.items()}
@@ -189,7 +211,7 @@ def load_biomedical(data_path='data/Biomedical/'):
         del emb_index
         del emb_vec
 
-    with open(data_path + 'Biomedical.txt', 'r') as inp_txt:
+    with open(data_path + "Biomedical.txt", "r", encoding="utf-8") as inp_txt:
         all_lines = inp_txt.readlines()[:-1]
         # print(sum([len(line.split()) for line in all_lines])/20000) #avg length
         text_file = " ".join([" ".join(nltk.word_tokenize(c)) for c in all_lines])
@@ -203,7 +225,7 @@ def load_biomedical(data_path='data/Biomedical/'):
         for i, line in enumerate(all_lines):
             word_sentence = nltk.word_tokenize(line)
 
-            sent_rep = np.zeros(shape=[48, ])
+            sent_rep = np.zeros(shape=(48,))
             j = 0
             for word in word_sentence:
                 try:
@@ -211,32 +233,39 @@ def load_biomedical(data_path='data/Biomedical/'):
                     j = j + 1
                 except KeyError:
                     continue
-
-                weight = 0.1 / (0.1 + unigram[word])
-                sent_rep += wv * weight
+                if use_SIF:
+                    weight = 0.1 / (0.1 + unigram[word])
+                    sent_rep += wv * weight
+                else:
+                    sent_rep += wv
             if j != 0:
                 all_vector_representation[i] = sent_rep / j
             else:
                 all_vector_representation[i] = sent_rep
 
-    svd = TruncatedSVD(n_components=1, random_state=rand_seed, n_iter=20)
-    svd.fit(all_vector_representation)
-    svd = svd.components_
-    XX = all_vector_representation - all_vector_representation.dot(svd.transpose()) * svd
+    if use_SIF:
+        svd = TruncatedSVD(n_components=1, random_state=rand_seed, n_iter=20)
+        svd.fit(all_vector_representation)
+        svd = svd.components_
+        XX = (
+            all_vector_representation
+            - all_vector_representation.dot(svd.transpose()) * svd
+        )
 
-    scaler = MinMaxScaler()
-    XX = scaler.fit_transform(XX)
-
+        scaler = MinMaxScaler()
+        XX = scaler.fit_transform(XX)
+    else:
+        XX = all_vector_representation
     return XX, y
 
 
-def load_data(dataset_name):
-    print('load data')
-    if dataset_name == 'stackoverflow':
-        return load_stackoverflow()
-    elif dataset_name == 'biomedical':
-        return load_biomedical()
-    elif dataset_name == 'search_snippets':
-        return load_search_snippet2()
+def load_data(dataset_name, use_SIF=True):
+    print("load data")
+    if dataset_name == "stackoverflow":
+        return load_stackoverflow(use_SIF)
+    elif dataset_name == "biomedical":
+        return load_biomedical(use_SIF)
+    elif dataset_name == "searchSnippets":
+        return load_search_snippet2(use_SIF)
     else:
-        raise Exception('dataset not found...')
+        raise Exception("dataset not found...")
